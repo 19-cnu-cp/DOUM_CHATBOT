451554ee4d8a64fda6598d27b1fcee2f65641600
jongkukpark <jongkukpark162@cnu.ac.kr>
2019-10-24 22:58:26 +0900

    drCorpAddr의 테스트 코드 추가

--- _test.py
+++ _test.py
@@ -1,16 +1,19 @@
 # 챗봇의 대강스러운 작동 원리를 테스트한다.
 
-from chatbotpack.nlubringer import goNlu
+from chatbotpack.nlubringer import goNlu, NluInfo
 from chatbotpack.dialog import DialogManager
 from doumdoum_dialog import DoumdoumDialogStrategy
-
+from testd.makeDict import MakeDict
+'''
 qtext = '컴투스 연매출 얼마야'
 #qtext = '컴투스 대표자 누구니'
 na = goNlu(qtext)
-
+'''
 dm = DialogManager( DoumdoumDialogStrategy() )
 #myMeta = {'nickname':'doumdoum_gigagenie_@_128.159.0.0'}
 myMeta = {}
+nluDict = MakeDict('컴투스 회사위치가 어디야', 'recruit.corpAddr', 3).getDict()
+na = NluInfo(nluDict)
 dr = dm.goDialog(myMeta, na)
 
 print('----------------------------')
